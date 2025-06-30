0b8127f804f0aaa59196e6506f25c17b1c4a88ec
Emil Mammadzada <66206088+emillon779@users.noreply.github.com>
2025-06-30 18:44:55 +0300

    fuelbuilder.py

--- fuelbuilder.py
+++ fuelbuilder.py
@@ -1,5 +1,18 @@
 import json
+# % --- Fuel with Gd: Serpent Mamual PDf Page-140
+# mat fuel7 -10.291
+# 92235.09c -3.13109E-02
+# 92238.09c -8.14929E-01
+# 64152.09c -6.70544E-05
+# 64154.09c -7.13344E-04
+# 64155.09c -5.06012E-03
+# 64156.09c -7.08860E-03
+# 64157.09c -5.43718E-03
+# 64158.09c -8.64341E-03
+# 64160.09c -7.69426E-03
+# 8016.09c -1.19056E-01
 
+# https://www.nist.gov/pml/atomic-weights-and-isotopic-compositions-relative-atomic-masses
 # JSON file read isotopes data
 with open('nistdataiso.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
